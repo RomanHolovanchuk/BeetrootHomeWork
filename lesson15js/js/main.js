d9ac6c990f5363f65f1e51b3012255f70b095ad8
RomanHolovanchuk <ro090986grs@gmail.com>
2022-06-02 10:07:10 +0300

    js15

---
+++ lesson15js/js/main.js
@@ -0,0 +1,37 @@
+// let 123age = 10;
+// let usd/eur = 0.93;
+
+/*let my-age = 35;
+let let = 10;*/
+
+
+
+let names=prompt('Enter your name:');
+console.log('Your name: ', names);
+
+const years=2022;
+let age=prompt('what year were you born?');
+let yearOfBirth = years - age;
+console.log('You are', yearOfBirth, 'years old');
+
+let oneSide = prompt('Enter one square side size:');
+let squareSide = oneSide * 4;
+console.log('Square side:', squareSide);
+
+
+const pi = 3.14;
+let r = prompt('Enter radius area:');
+let area = r * pi;
+console.log('Area:', area);
+
+let distance = prompt('what is the distance between the cities in km?');
+let time = prompt ('How many hours you wants to get the cities?');
+let speed = distance / time;
+console.log('your speed should be', speed, 'km/h');
+
+const usd_eur = 0.93;
+let usd = prompt('how many dollars do you have?');
+let eur = usd*usd_eur;
+console.log('You have', eur, 'euro');
+
+
--- lesson15js/main.js
+++
@@ -1,9 +0,0 @@
-const burgerButton = document.getElementById("burger");
-const closeButton = document.getElementById("close");
-const mobileMenu = document.getElementById("mobile-menu");
-burgerButton.addEventListener("click", function handleClick() {
-  mobileMenu.style.display = "flex";
-});
-closeButton.addEventListener("click", function handleClick() {
-  mobileMenu.style.display = "none";
-});
